b8968d0ffe669ca09562f1028252d0d2b158efb1
Jenni <jenni@ohsomodern.org>
2023-01-11 15:31:09 +0200

    show how to do things

--- src/components/ImagePlay.vue
+++ src/components/ImagePlay.vue
@@ -1,17 +1,19 @@
 <template>
     <div>
+        <h2>ImagePlay.vue</h2>
+        <pre>
+            {{ imageData }}
+        </pre>
         <button> </button>
-        <img: src="''+images[]" alt="recepies[]alt" width="300" height="200" loading="lazy">
+        <img: src="''+images[]" alt="recepies[]alt" width="300" height="200" loading="lazy" />
         <button> </button>
 
 
     </div>
 </template>
 
-<script lang="ts">
-    export default {
-        
-    }
+<script setup lang="ts">
+const props = defineProps(['imageData']);
 </script>
 
 <style lang="sass" scoped>
